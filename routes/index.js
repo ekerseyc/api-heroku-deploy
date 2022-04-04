27ff8dc71a03966011e74720fb5907230d2646b0
Emily Kersey <ekerseyc@gmail.com>
2022-04-04 10:10:23 -0400

    hehe

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,98 @@
 const router = require('express').Router();
+const { Location, Traveller, Trip } = require('../models');
 
-router.get('/api/travellers', (req, res) => {
-    res.json({});
+// travellers
+router.get('/api/travellers', async (req, res) => {
+    const travellerData = await Traveller.findAll({});
+    res.json(travellerData);
 })
 
-router.post('/api/travellers', (req, res) => {
-    res.json({});
+router.post('/api/travellers', async (req, res) => {
+    const travellerData = await Traveller.findAll({});
+    res.json(travellerData);
 })
 
-router.get('/api/travellers/:id', (req, res) => {
-    res.json({});
+router.get('/api/travellers/:id', async (req, res) => {
+    const travellerData = await Traveller.findByPk(
+        req.params.id, {
+        include: [
+            {
+                model: Location,
+                through: {
+                    attributes: []
+                }}]
+    }
+    );
+    res.json(travellerData);
 })
 
-router.delete('/api/travellers/:id', (req, res) => {
-    res.json({});
+router.delete('/api/travellers/:id', async (req, res) => {
+    const travellerData = await Traveller.destroy({
+        where: {
+            id: req.params.id
+        }
+    });
+    res.json(travellerData);
 })
 
-router.get('/api/locations', (req, res) => {
-    res.json({});
+// locations
+router.get('/api/locations', async (req, res) => {
+    const locationData = await Location.findAll({});
+    res.json(locationData);
 })
 
-router.post('/api/locations', (req, res) => {
-    res.json({});
+router.post('/api/locations', async (req, res) => {
+    const locationData = await Location.create(req.body);
+    res.json(locationData);
 })
 
-router.get('/api/locations/:id', (req, res) => {
-    res.json({});
+router.get('/api/locations/:id', async (req, res) => {
+    const locationData = await Location.findByPk(
+        req.params.id,
+        {
+            include: [{
+                model: Traveller,
+                through: Trip
+            }]
+        }
+    );
+    const location = JSON.parse(JSON.stringify(locationData));
+
+    const updated = location.travellers.forEach(traveller => {
+        delete traveller.trip;
+        return traveller;
+    });
+    console.log(location);
+    res.json(location);
 })
 
-router.delete('/api/locations/:id', (req, res) => {
-    res.json({});
+router.delete('/api/locations/:id', async (req, res) => {
+    const locationData = await Location.destroy({
+        where: {
+            id: req.params.id
+        }
+    });
+    res.json(locationData);
 })
 
-router.post('/api/trips', (req, res) => {
-    res.json({});
-})
+// TRIPS
+router.get('/api/trips', async (req, res) => {
+    const tripData = await Trip.findAll({});
+    res.json(tripData);
+});
 
-router.delete('/api/trips', (req, res) => {
-    res.json({});
-})
+router.post('/api/trips', async (req, res) => {
+    const tripData = await Trip.create(req.body);
+    res.json(tripData);
+});
+
+router.delete('/api/trips/:id', async (req, res) => {
+    const tripData = await Trip.destroy({
+        where: {
+            id: req.params.id
+        }
+    });
+    res.json(tripData);
+});
 
 module.exports = router;
